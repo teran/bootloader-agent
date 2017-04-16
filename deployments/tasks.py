1cc9adfaabb2125421de6c129f9fdf2c4c032036
Igor Shishkin <me@teran.ru>
2017-04-16 17:50:53 +0300

    Using python3-friendly syntax

--- deployments/tasks.py
+++ deployments/tasks.py
@@ -15,34 +15,34 @@ logger.basicConfig(format=settings.LOG_FORMAT)
 
 @app.task
 def deployment_created(*args, **kwargs):
-    print args, kwargs
+    print(args, kwargs)
 
 
 @app.task
 def evaluate_new(*args, **kwargs):
-    print args, kwargs
+    print(args, kwargs)
 
 
 @app.task
 def evaluate_preparing(*args, **kwargs):
-    print args, kwargs
+    print(args, kwargs)
 
 
 @app.task
 def evaluate_installing(*args, **kwargs):
-    print args, kwargs
+    print(args, kwargs)
 
 
 @app.task
 def evaluate_configuring(*args, **kwargs):
-    print args, kwargs
+    print(args, kwargs)
 
 
 @app.task
 def evaluate_postconfiguring(*args, **kwargs):
-    print args, kwargs
+    print(args, kwargs)
 
 
 @app.task
 def evaluate_error(*args, **kwargs):
-    print args, kwargs
+    print(args, kwargs)
