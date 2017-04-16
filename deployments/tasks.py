6b9a13081269b4229b630a27337bdbc1c83f457a
Igor Shishkin <me@teran.ru>
2017-04-16 17:31:13 +0300

    Tasks interface updated according to new scheme

--- deployments/tasks.py
+++ deployments/tasks.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 
 import logging as logger
-import os
 
 from celery import Celery
 
-import api
 import settings
 
 
@@ -16,34 +14,35 @@ logger.basicConfig(format=settings.LOG_FORMAT)
 
 
 @app.task
-def deployment_start(deployment):
-    deployment_object = api.get_deployment(
-        deployment_id=deployment)
-    profile_object = api.get_profile(
-        profile_id=deployment_object.get('profile'))
-
-    fileBase = '%sexport/file/%s/%s/%s/%s' % (
-        settings.BOOTLOADER_URL,
-        deployment,
-        deployment_object.get('token'),
-        profile_object.get('name'),
-        profile_object.get('version'))
-
-    interfaces_object = api.get_interfaces_by_server(
-        deployment_object.get('server'))
-    for interface in interfaces_object:
-        download_file.apply_async(
-            args=[
-                '%s/%s' % (fileBase, 'pxelinux.cfg'),
-                '/var/lib/tftp/pxelinux.cfg/01-%s' % interface.get(
-                    'mac').replace(':', '-')])
+def deployment_created(*args, **kwargs):
+    print args, kwargs
 
 
 @app.task
-def download_file(URL, target):
-    api.download_file(URL, target)
+def evaluate_new(*args, **kwargs):
+    print args, kwargs
 
 
 @app.task
-def delete_file(target):
-    os.unlink(target)
+def evaluate_preparing(*args, **kwargs):
+    print args, kwargs
+
+
+@app.task
+def evaluate_installing(*args, **kwargs):
+    print args, kwargs
+
+
+@app.task
+def evaluate_configuring(*args, **kwargs):
+    print args, kwargs
+
+
+@app.task
+def evaluate_postconfiguring(*args, **kwargs):
+    print args, kwargs
+
+
+@app.task
+def evaluate_error(*args, **kwargs):
+    print args, kwargs
